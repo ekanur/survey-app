03edd22da6ab7cc1f3545b02fd818c1e35356df3
Eka Nur Ahmad R <ekanur@um.ac.id>
2018-09-12 08:49:03 +0700

    dosen migration

---
+++ database/migrations/2018_09_12_012609_biodata_dosen.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BiodataDosen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('biodata_dosen', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("email", 75);
+            $table->string("prodi", 100);
+            $table->string("jurusan", 100);
+            $table->string("fakultas", 100);
+            $table->string("fakultas", 100);
+            $table->date("tmt");
+            $table->enum("jenis_kelamin", ["laki_laki", "perempuan"]);
+            $table->enum("usia", ["<30", "31-40", "41-50", "51-60", ">60"]);
+            $table->string("pendidikan_tertinggi", 30);
+            $table->string("lama_mengajar", 25);
+            $table->enum("jabatan_fungsional", ["TP", "AA", "L", "LK", "GB"]);
+            $table->string("tugas_tambahan", 50);
+            $table->timestamps();
+            $table->softDeletes();
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_09_12_013548_angket_dosen.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AngketDosen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('angket_dosen', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("biodata_dosen_id");
+            $table->string("kuesioner", 10);
+            $table->string("value", 200);
+            $table->timestamps();
+            $table->softDeletes();
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
