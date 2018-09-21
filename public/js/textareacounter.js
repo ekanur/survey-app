43e48f558013c5ef90853ae8217bda60c2f81836
Dimas Virdana <virdana10@gmail.com>
2018-09-21 10:10:43 +0700

    Fixing plugin words limiter

--- public/js/textareacounter.js
+++ public/js/textareacounter.js
@@ -15,14 +15,13 @@
     var options = $.extend(defaults, options);
  
     // and the plugin begins
-    console.log(this);
-    return this.each(function() {
+    return this.each(function(index, val) {
       var obj, text, wordcount, limited;
       
       obj = $(this);
-      obj.after('<i style="font-size: 15px; font-weight:400; color: #00b8d8; clear: both; margin-top: 3px; display: block;" id="counter-text">Maksimal '+options.limit+' kata</i>');
+      obj.after('<i style="font-size: 15px; font-weight:400; color: #00b8d8; clear: both; margin-top: 3px; display: block;" class="counter-label">Maksimal '+options.limit+' kata</i>');
 
-      obj.keyup(function() {
+      obj.keyup(function(e) {
           text = obj.val();
           if(text === "") {
             wordcount = 0;
@@ -30,12 +29,12 @@
             wordcount = $.trim(text).split(" ").length;
         }
           if(wordcount > options.limit) {
-              $("#counter-text").html('<span style="color: #DD0000;">0 kata tersisa</span>');
+              $(obj).next('.counter-label').html('<span style="color: #DD0000;">0 kata tersisa</span>');
           limited = $.trim(text).split(" ", options.limit);
           limited = limited.join(" ");
           $(this).val(limited);
           } else {
-              $("#counter-text").html((options.limit - wordcount)+' kata tersisa');
+              $(obj).next('.counter-label').html((options.limit - wordcount)+' kata tersisa');
           } 
       });
     });
