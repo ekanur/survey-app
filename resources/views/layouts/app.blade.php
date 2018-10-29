1ff1bff11f53065b786045edb23c8f48c9b9b414
Virdana <virdana10@gmail.com>
2018-10-29 07:17:42 +0700

    Fix Conflict on app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -146,6 +146,7 @@
 <script src="//cdn.jsdelivr.net/jquery/2.2.0/jquery.min.js"></script>
 <script src="//cdn.jsdelivr.net/jquery.sidr/2.2.1/jquery.sidr.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
 <script type="text/javascript">
     $(document).ready(function(){
         $("select[name='tahun']").change(function(){
